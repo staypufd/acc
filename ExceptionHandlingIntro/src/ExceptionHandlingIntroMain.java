7edd6338cc329256029170bba572c0e5455755ba
Sam Griffith Jr <staypufd@mac.com>
2014-07-08 11:26:00 -0500

    RuntimeExceptions example

---
+++ ExceptionHandlingIntro/src/ExceptionHandlingIntroMain.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author samjr
+ *
+ */
+public class ExceptionHandlingIntroMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer i = new Integer(10);
+		float f = i.floatValue();
+		ArrayList<String> a = null;
+		String valueToBeAdded = "Sam";
+		
+		try {
+			a = new ArrayList<>(1000000000);
+			a.add("Bob");
+			if (a != null) {
+				a.add(10, valueToBeAdded);
+			}
+		} 
+		catch (OutOfMemoryError oome) {
+			System.out.println("We're out of memory!");
+		}
+		catch (IndexOutOfBoundsException  e) {
+			System.out.println("Bummer! Couldn't add " + valueToBeAdded);
+			//a.add(a.size(), valueToBeAdded);
+		}
+		catch (java.lang.RuntimeException rte) {
+			System.out.println("Runtime Exception");
+		}
+		finally {
+			System.out.println("Clean up");
+			try {
+				a.clear();
+			}
+			catch (NullPointerException npe) {
+				System.out.println("Dang Nulls");
+			}
+		}
+		
+		System.out.println("a is: " + a);
+	}
+
+}
